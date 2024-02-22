9bcfead5e9a635bb1f9e9dbc0780cae33837db51
samrat014 <tarmaskarki014@gmail.com>
2024-02-22 21:26:35 +0545

    refactor: method name

--- routes/api.php
+++ routes/api.php
@@ -26,46 +26,46 @@ Route::post('/login', [AuthenticationController::class, 'login']);
 //Route::middleware('auth:sanctum')->group(function () {
 
     Route::prefix('customer')->group(function () {
-        Route::post('/', [UserController::class, 'index']);
+        Route::get('/', [UserController::class, 'index']);
         Route::post('/store', [UserController::class, 'store']);
         Route::post('/update/{id}', [UserController::class, 'update']);
-        Route::post('/delete/{id}', [UserController::class, 'destroy']);
+        Route::delete('/delete/{id}', [UserController::class, 'destroy']);
     });
 
     Route::prefix('brand')->group(function () {
-        Route::post('/', [BrandController::class, 'index']);
+        Route::get('/', [BrandController::class, 'index']);
         Route::post('/store', [BrandController::class, 'store']);
         Route::post('/update/{id}', [BrandController::class, 'update']);
-        Route::post('/delete/{id}', [BrandController::class, 'destroy']);
+        Route::delete('/delete/{id}', [BrandController::class, 'destroy']);
     });
 
     Route::prefix('category')->group(function () {
-        Route::post('/', [CategoryController::class, 'index']);
+        Route::get('/', [CategoryController::class, 'index']);
         Route::post('/store', [CategoryController::class, 'store']);
         Route::post('/update/{id}', [CategoryController::class, 'update']);
-        Route::post('/delete/{id}', [CategoryController::class, 'destroy']);
+        Route::delete('/delete/{id}', [CategoryController::class, 'destroy']);
     });
 
     Route::prefix('product')->group(function () {
-        Route::post('/', [ProductController::class, 'index']);
+        Route::get('/', [ProductController::class, 'index']);
         Route::post('/store', [ProductController::class, 'store']);
         Route::post('/update/{id}', [ProductController::class, 'update']);
-        Route::post('/delete/{id}', [ProductController::class, 'destroy']);
+        Route::delete('/delete/{id}', [ProductController::class, 'destroy']);
     });
 
     // this are called product out as per client requirement
     Route::prefix('sale')->group(function () {
-        Route::post('/', [SaleController::class, 'index']);
+        Route::get('/', [SaleController::class, 'index']);
         Route::post('/store', [SaleController::class, 'store']);
         Route::post('/update/{id}', [SaleController::class, 'update']);
-        Route::post('/delete/{id}', [SaleController::class, 'destroy']);
+        Route::delete('/delete/{id}', [SaleController::class, 'destroy']);
     });
 
     Route::prefix('expense-category')->group(function () {
-        Route::post('/', [ExpenseCategoryController::class, 'index']);
+        Route::get('/', [ExpenseCategoryController::class, 'index']);
         Route::post('/store', [ExpenseCategoryController::class, 'store']);
         Route::post('/update/{expenseCategory}', [ExpenseCategoryController::class, 'update']);
-        Route::post('/delete/{expenseCategory}', [ExpenseCategoryController::class, 'destroy']);
+        Route::delete('/delete/{expenseCategory}', [ExpenseCategoryController::class, 'destroy']);
     });
 
 //});
