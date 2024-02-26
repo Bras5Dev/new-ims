5f6534d204f7aa528a5fbb079e5110db4402c8e7
samrat014 <tarmaskarki014@gmail.com>
2024-02-26 22:54:31 +0545

    add: log while user fails to login

--- app/Http/Controllers/Api/AuthenticationController.php
+++ app/Http/Controllers/Api/AuthenticationController.php
@@ -26,6 +26,7 @@ class AuthenticationController extends Controller
                 ->first();
 
             if (!$user || !Hash::check($password, $user->password)) {
+                Log::info('User failed to login.', ['email' => $request['email'], 'id' => $user->id ?? 'N/A']);
                 return response()->json(['message' => 'Your email or password does not match'], 422);
             }
 
