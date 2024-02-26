edd8779488346c5212bfd9cf848662eba4c5b29c
samrat014 <tarmaskarki014@gmail.com>
2024-02-26 16:53:11 +0545

    fix: model binding

--- routes/api.php
+++ routes/api.php
@@ -42,23 +42,23 @@ Route::prefix('brands')->group(function () {
 Route::prefix('category')->group(function () {
     Route::get('/', [CategoryController::class, 'index']);
     Route::post('/store', [CategoryController::class, 'store']);
-    Route::post('/update/{id}', [CategoryController::class, 'update']);
-    Route::delete('/delete/{id}', [CategoryController::class, 'destroy']);
+    Route::post('/update/{category}', [CategoryController::class, 'update']);
+    Route::delete('/delete/{category}', [CategoryController::class, 'destroy']);
 });
 
 Route::prefix('product')->group(function () {
     Route::get('/', [ProductController::class, 'index']);
     Route::post('/store', [ProductController::class, 'store']);
-    Route::post('/update/{id}', [ProductController::class, 'update']);
-    Route::delete('/delete/{id}', [ProductController::class, 'destroy']);
+    Route::post('/update/{product}', [ProductController::class, 'update']);
+    Route::delete('/delete/{product}', [ProductController::class, 'destroy']);
 });
 
 // this are called product out as per client requirement
 Route::prefix('sale')->group(function () {
     Route::get('/', [SaleController::class, 'index']);
     Route::post('/store', [SaleController::class, 'store']);
-    Route::post('/update/{id}', [SaleController::class, 'update']);
-    Route::delete('/delete/{id}', [SaleController::class, 'destroy']);
+    Route::post('/update/{sale}', [SaleController::class, 'update']);
+    Route::delete('/delete/{sale}', [SaleController::class, 'destroy']);
 });
 
 Route::prefix('expense-category')->group(function () {
